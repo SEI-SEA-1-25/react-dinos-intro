372652d479cde91239bb956345af6c08ce4185b6
Colin Jaffe <balloonasaurus@gmail.com>
2021-03-08 22:05:52 -0500

    Adds alternate looping mechanisms and removes example Post call.

--- src/Post.js
+++ src/Post.js
@@ -1,33 +1,42 @@
-import {Component} from 'react';
+import { Component } from "react";
 
 class Header extends Component {
   render() {
-    return (<h1>{this.props.header}</h1>)
+    return <h1>{this.props.header}</h1>;
   }
 }
 
 class Author extends Component {
   render() {
-    return (<h3>{this.props.author}</h3>)
+    return <h3>{this.props.author}</h3>;
   }
 }
 
 class Content extends Component {
   render() {
-    return (<p>{this.props.content}</p>)
+    return <p>{this.props.content}</p>;
   }
 }
 
 class Comment extends Component {
   render() {
-    return (<p>{this.props.comment}</p>)
+    return <p>{this.props.comment}</p>;
   }
 }
 
 class Post extends Component {
-  render () {
+  render() {
     const comments = [];
-    this.props.comments.forEach((comment) => comments.push(<Comment comment={comment} />))
+    this.props.comments.forEach((comment) => comments.push(<Comment comment={comment} />));
+
+    // Instead of the forEach, you could use a for loop
+    // to iterate through this.props.comments:
+    // for (const comment of this.props.comments) {
+    //   comments.push(<Comment comment={comment} />);
+    // }
+
+    // Or, instead of any of the above, you could map it:
+    // const comments = this.props.comments.map((comment) => <Comment comment={comment} />)
 
     return (
       <div>
@@ -36,21 +45,21 @@ class Post extends Component {
         <Content content={this.props.content} />
         <p>{comments}</p>
       </div>
-    )
+    );
   }
 }
 
+// A locked post has everything a post has, without the comments section.
 class LockedPost extends Component {
-  render () {
+  render() {
     return (
       <div>
         <Header header={this.props.header} />
         <Author author={this.props.author} />
         <Content content={this.props.content} />
       </div>
-    )
+    );
   }
 }
-<Post comments={['one comment', 'two']} style=""></Post>
 
 export default Post;
